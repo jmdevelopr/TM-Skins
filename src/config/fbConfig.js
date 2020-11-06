9f259c4611bc5e71ca8fd604064260bc56dadc04
Jakub Makuch <57146955+jmdevelopr@users.noreply.github.com>
2020-11-06 12:34:49 +0100

    Update fbConfig.js

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,13 +4,6 @@ import 'firebase/storage';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyDVRyoG7y15p7bs80Scwn2wiB7h6IxRI2c",
-  authDomain: "trackmaniaskins.firebaseapp.com",
-  databaseURL: "https://trackmaniaskins.firebaseio.com",
-  projectId: "trackmaniaskins",
-  storageBucket: "trackmaniaskins.appspot.com",
-  messagingSenderId: "870147414424",
-  appId: "1:870147414424:web:2ce9a8c9b96156720d73ed"
 };
 
 // Initialize Firebase
